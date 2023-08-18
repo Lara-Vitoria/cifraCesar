5d2359988dc8e230fd5ba40ff9df40d3ff116032
Lara Vitoria <74455275+Lara-Vitoria@users.noreply.github.com>
2023-08-18 13:57:01 -0300

    adicionado descriptografia

--- CriptografiaCesar.py
+++ CriptografiaCesar.py
@@ -14,3 +14,16 @@ class CriptografiaCesar:
                 mensagemCriptografada += letra
 
         return mensagemCriptografada
+    
+    def descriptografa(self, mensagem,  chave = 3):
+        mensagemDescriptografada = ''
+        mensagem = mensagem.upper()
+
+        for letra in mensagem:
+            if letra in self.__alfabeto:
+                index = (self.__alfabeto.find(letra) - chave) % 26 
+                mensagemDescriptografada += self.__alfabeto[index]
+            else:
+                mensagemDescriptografada += letra
+
+        return mensagemDescriptografada.lower()
