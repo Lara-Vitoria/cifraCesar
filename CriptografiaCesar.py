91f15be5b202c2e6281a434374a3156aaf906ac2
Lara Vitoria <74455275+Lara-Vitoria@users.noreply.github.com>
2023-08-19 14:30:02 -0300

    descriptografia ajustada

--- CriptografiaCesar.py
+++ CriptografiaCesar.py
@@ -1,14 +1,17 @@
 class CriptografiaCesar:
     def __init__(self):
-        self.__alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        self.__alfabeto = ''
  
     def criptografa(self, mensagem, chave = 3):
         mensagemCriptografada = ''
         mensagem = mensagem.upper()
 
+        for i in range(128):
+            self.__alfabeto += chr(i)
+
         for letra in mensagem:
             if letra in self.__alfabeto:
-                index = (self.__alfabeto.find(letra) + chave) % 26 
+                index = (self.__alfabeto.find(letra) + chave) % 128 
                 mensagemCriptografada += self.__alfabeto[index]
             else:
                 mensagemCriptografada += letra
@@ -17,11 +20,14 @@ class CriptografiaCesar:
     
     def descriptografa(self, mensagem,  chave = 3):
         mensagemDescriptografada = ''
-        mensagem = mensagem.upper()
+        mensagem = mensagem.upper().decode('utf-8')
+
+        for i in range(128):
+            self.__alfabeto += chr(i)
 
         for letra in mensagem:
             if letra in self.__alfabeto:
-                index = (self.__alfabeto.find(letra) - chave) % 26 
+                index = (self.__alfabeto.find(letra) - chave) % 128 
                 mensagemDescriptografada += self.__alfabeto[index]
             else:
                 mensagemDescriptografada += letra
